d8ed29cb3b690900b0526fc8ddefe9e069def0c7
Jaskaran Singh <jaskaran1523@gmail.com>
2022-10-28 22:22:45 -0700

    Remove boilerplate code

---
+++ pages/index.js
@@ -0,0 +1,15 @@
+import Head from 'next/head';
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>CoderSingh Eats</title>
+        <meta name="description" content="CoderSingh Eats" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main>CoderSingh Eats</main>
+    </div>
+  );
+}
