14a15e00e02f68e4cb36c716962d168a5dec21c1
Jaskaran Singh <jaskaran1523@gmail.com>
2022-11-02 01:30:58 -0700

    Separate banner component

---
+++ components/Banner.jsx
@@ -0,0 +1,43 @@
+import Image from 'next/image';
+import { Box, Button, Flex, Text } from '@chakra-ui/react';
+
+import Food from '../assets/foodbg.jpg';
+
+const Banner = () => (
+  <Box position="relative" height="calc(100vh - 70px)" overflow="hidden">
+    <Image src={Food} alt="Food Background" />
+
+    <Flex
+      background="rgba(51, 51, 51, 0.85)"
+      position="absolute"
+      height="100vh"
+      width="100vw"
+      top={0}
+      left={0}
+      flexDir="column"
+      justifyContent="center"
+      alignItems="center"
+      textTransform="uppercase"
+    >
+      <Text fontSize="2em" color="#fff">
+        Welcome to
+      </Text>
+      <Text fontSize="4em" color="#fff" fontWeight="bold">
+        The CoderSingh Eats
+      </Text>
+      <Flex gap={4}>
+        <Button borderRadius="0" textTransform="uppercase">
+          Menu
+        </Button>
+        <Button borderRadius="0" textTransform="uppercase">
+          Book a Table
+        </Button>
+        <Button borderRadius="0" textTransform="uppercase">
+          Order Takeout
+        </Button>
+      </Flex>
+    </Flex>
+  </Box>
+);
+
+export default Banner;
