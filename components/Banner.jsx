8e816dc0719fd0d235e0eb7d0e144d6aff5a6494
Jaskaran Singh <jaskaran1523@gmail.com>
2022-11-03 01:26:19 -0700

    Make banner responsive

--- components/Banner.jsx
+++ components/Banner.jsx
@@ -5,7 +5,11 @@ import Food from '../assets/foodbg.jpg';
 
 const Banner = () => (
   <Box position="relative" height="calc(100vh - 70px)" overflow="hidden">
-    <Image src={Food} alt="Food Background" />
+    <Image
+      src={Food}
+      alt="Food Background"
+      style={{ objectFit: 'cover', height: '100%' }}
+    />
 
     <Flex
       background="rgba(51, 51, 51, 0.85)"
@@ -19,13 +23,22 @@ const Banner = () => (
       alignItems="center"
       textTransform="uppercase"
     >
-      <Text fontSize="2em" color="#fff">
+      <Text fontSize={['0.8em', '1.2em', '1.7em', '2em']} color="#fff">
         Welcome to
       </Text>
-      <Text fontSize="4em" color="#fff" fontWeight="bold">
+      <Text
+        fontSize={['2em', '2.5em', '3.5em', '4em']}
+        color="#fff"
+        fontWeight="bold"
+      >
         The CoderSingh Eats
       </Text>
-      <Flex gap={4}>
+      <Flex
+        gap={4}
+        direction={{ base: 'column', md: 'row' }}
+        mt={2}
+        w={{ base: '60%', sm: '60%', md: 'unset' }}
+      >
         <Button borderRadius="0" textTransform="uppercase">
           Menu
         </Button>
