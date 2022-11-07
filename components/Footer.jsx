f3a8fb8d1e50cb5bb113c4abc26bfb5ad2bdc0f8
Jaskaran Singh <jaskaran1523@gmail.com>
2022-11-07 00:23:53 -0800

    Create footer

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,3 +1,17 @@
-const Footer = () => <div>Footer</div>;
+import { Box, Text } from '@chakra-ui/react';
+import Link from 'next/link';
+
+const Footer = () => (
+  <Box mt={20} textAlign="center" bg="#333" py={2}>
+    <Text fontSize="0.8em">
+      Copyright &copy; 2022 | CoderSingh Eats | All Rights Reserved | Designed &
+      Developed by
+      <Link href="https://thecodersingh.com/" target="_blank">
+        {' '}
+        TheCoderSingh{' '}
+      </Link>
+    </Text>
+  </Box>
+);
 
 export default Footer;
