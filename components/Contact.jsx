cb949934e90e2acee71a8ffa1384e81a872dbd25
Jaskaran Singh <jaskaran1523@gmail.com>
2022-11-07 00:18:15 -0800

    Create contact form

--- components/Contact.jsx
+++ components/Contact.jsx
@@ -1,5 +1,70 @@
-import { Box } from '@chakra-ui/react';
+import {
+  Box,
+  Button,
+  FormControl,
+  Heading,
+  Input,
+  Textarea
+} from '@chakra-ui/react';
 
-const Contact = () => <Box>Contact</Box>;
+const Contact = () => (
+  <Box>
+    <Heading textTransform="uppercase" pt={20} textAlign="center">
+      Contact Us
+    </Heading>
+
+    <Box mt={20} maxW="50%" mx="auto">
+      <form>
+        <FormControl>
+          <Input
+            placeholder="Name"
+            borderTop="none"
+            borderX="none"
+            borderRadius="none"
+            isRequired
+          />
+        </FormControl>
+        <FormControl mt={10}>
+          <Input
+            placeholder="Phone"
+            borderTop="none"
+            borderX="none"
+            borderRadius="none"
+            isRequired
+          />
+        </FormControl>
+        <FormControl mt={10}>
+          <Input
+            placeholder="Email"
+            type="email"
+            borderTop="none"
+            borderX="none"
+            borderRadius="none"
+            isRequired
+          />
+        </FormControl>
+        <FormControl mt={10}>
+          <Textarea
+            placeholder="Message"
+            borderTop="none"
+            borderX="none"
+            borderRadius="none"
+            minH="unset"
+            resize="none"
+            isRequired
+          />
+        </FormControl>
+        <Button
+          type="submit"
+          borderRadius="none"
+          mt={10}
+          textTransform="uppercase"
+        >
+          Send Message
+        </Button>
+      </form>
+    </Box>
+  </Box>
+);
 
 export default Contact;
